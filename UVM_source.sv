cf493d6fb3e0939abc6395247fa28b9a217d449f
suresh2327 <sureshgadi68@gmail.com>
2026-02-18 10:57:55 +0530

    UVM

--- UVM_source.sv
+++ UVM_source.sv
@@ -1757,6 +1757,92 @@ L: UVM_INFO @ 0: reporter [RNTST] Running test test...
 //TLM port communication using put_port , put_export and put_imp
 //where the memory for all tlm ports are created in custom constrcur it self not in build phase
 
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class producer extends uvm_component;
+  `uvm_component_utils(producer);
+   int data=10; 
+  
+  uvm_blocking_put_port#(int) send;
+  
+  function new(string path="producer", uvm_component parent=null);
+    super.new(path,parent);
+    send=new("send",this);
+  endfunction
+  
+  task main_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    send.put(data);
+    `uvm_info("send",$sformatf("data sent=%0d",data),UVM_NONE);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+class consumer extends uvm_component;
+  `uvm_component_utils(consumer);
+  uvm_blocking_put_export#(int)rec;
+  uvm_blocking_put_imp#(int, consumer) imp;
+  function new(string path="consumer", uvm_component parent=null);
+    super.new(path,parent);
+    rec=new("rec",this);
+    imp=new("imp",this);
+  endfunction
+  task put(int rdata);
+    `uvm_info("CONSUMER",$sformatf("data recived=%0d",rdata),UVM_NONE);
+  endtask
+             
+endclass
+
+class env extends uvm_env;
+  `uvm_component_utils(env);
+  producer p;
+  consumer c;
+  
+  function new(string path="env", uvm_component parent=null);
+    super.new(path,parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    p=producer::type_id::create("p",this);
+    c=consumer::type_id::create("c",this);
+  endfunction
+  
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    p.send.connect(c.rec);
+    c.rec.connect(c.imp);
+  endfunction
+  
+  
+endclass
+
+class test extends uvm_test;
+  `uvm_component_utils(test);
+  env e;
+  function new(string path="test", uvm_component parent=null);
+    super.new(path,parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    e=env::type_id::create("e",this);
+  endfunction
+endclass
+
+module top;
+  initial
+    begin
+      run_test("test");
+    end
+endmodule
+
+//output
+# KERNEL: UVM_INFO /home/runner/testbench.sv(34) @ 0: uvm_test_top.e.c [CONSUMER] data recived=10
+# KERNEL: UVM_INFO /home/runner/testbench.sv(19) @ 0: uvm_test_top.e.p [send] data sent=10
+
+
 
 
 
